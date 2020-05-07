346c95017e15edd5c4f881fa098b0d889f407904
skj1991 <stacykjohns@icloud.com>
2020-05-07 18:02:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,52 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/' do
+    "Welcome to the Magazine App!"
+  end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index 
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params["title"]
+    @article.content = params["content"]
+    @article.save
+    redirect to("/articles/#{@article.id}")
+  end
+
+  post '/articles' do 
+    @article = Article.new
+    @article.title = params["title"]
+    @article.content = params["content"]
+    @article.save
+    redirect to("/articles/#{@article.id}")
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end 
+
+  delete '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect to("/articles")
   end
 end
+
